f34713e26266596c7c51d7110b05d39c7380a087
Oleksandr Khomyakov <alectix@gmail.com>
2015-05-27 13:03:38 +0300

    After rubocop insp

--- t1.rb
+++ t1.rb
@@ -1,11 +1,11 @@
-print "Enter number: "
+print 'Enter number: '
 n = Integer(gets.chomp)
 puts "Number #{n}"
 counter = 1
 
-while(n>1)
-	n/=2
-	counter+=1
+while n > 1
+  n /= 2
+  counter += 1
 end
 
-puts "Smallest pow of 2 is: #{counter}" 
+puts "Smallest pow of 2 is: #{counter}"
--- t2.rb
+++ t2.rb
@@ -1,18 +1,18 @@
-puts "Vvedit' n: "
+puts 'Vvedit\' n: '
 n = Integer(gets.chomp)
 
 def fun(n)
-	for x in 1..n
-		for y in 1..n
-			for z in 1..n
-				if n==(x**2)+(y**2)+(z**2)
-					puts "x=#{x} y=#{y} z=#{z}"
-					return
-				end
-			end
-		end
-	end
-	puts "Ne moshna predstavyty v vyhlyadi sumy 3kh naturalnykh kvadrativ!"
+  for x in 1..n
+    for y in 1..n
+      for z in 1..n
+        if n == (x**2) + (y**2) + (z**2)
+          puts "x=#{x} y=#{y} z=#{z}"
+          return
+        end
+      end
+    end
+  end
+  puts 'Ne moshna predstavyty v vyhlyadi sumy 3kh naturalnykh kvadrativ!'
 end
 
 fun(n)
