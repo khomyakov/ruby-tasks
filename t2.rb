4ebbfabbdf7b843dac01bfb21b74c5f01a806f9e
Oleksandr Khomyakov <alectix@gmail.com>
2015-05-27 14:07:32 +0300

    All code conv

--- t2.rb
+++ t2.rb
@@ -2,17 +2,14 @@ puts 'Vvedit\' n: '
 n = Integer(gets.chomp)
 
 def fun(n)
-  for x in 1..n
-    for y in 1..n
-      for z in 1..n
-        if n == (x**2) + (y**2) + (z**2)
-          puts "x=#{x} y=#{y} z=#{z}"
-          return
-        end
+  n.times do |x|
+    n.times do |y|
+      n.times do |z|
+        return "x=#{x} y=#{y} z=#{z}" if n == (x**2) + (y**2) + (z**2)
       end
     end
   end
   puts 'Ne moshna predstavyty v vyhlyadi sumy 3kh naturalnykh kvadrativ!'
 end
 
-fun(n)
+puts fun(n)
